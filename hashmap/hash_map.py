32e37c0234ec8864de7ef3370c41946cc5210978
shubhi31 <mittal.sh@husky.neu.edu>
2019-03-29 23:32:40 -0400

    Initial API to be developed for hashMap

---
+++ hashmap/hash_map.py
@@ -0,0 +1,33 @@
+"""
+Implementing HashMap API
+"""
+
+
+class HashMap(object):
+
+    def __init__(self):
+        pass
+
+    def __hashcode(self, key):
+        pass
+
+    def put(self, key, value):
+        pass
+
+    def get(self, key):
+        pass
+
+    def contains(self, key):
+        pass
+
+    def remove(self, key):
+        pass
+
+    def size(self):
+        pass
+
+    def equals(self, obj):
+        pass
+
+    def __str__(self):
+        pass
---
+++ hashmap/tests.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class HashMapTest(TestCase):
+    pass
+
